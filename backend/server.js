97971542460adf409f38133f9ab7a16f20b3dd36
dami <devdami0828@gmail.com>
2024-04-16 00:44:56 +0900

    change

--- backend/server.js
+++ backend/server.js
@@ -49,6 +49,6 @@ app.post("/api/value", function (req, res, next) {
   );
 });
 
-app.listen(5000, () => {
-  console.log("애플리케이션이 5000번 포트에서 시작되었습니다.");
+app.listen(5010, () => {
+  console.log("애플리케이션이 5010번 포트에서 시작되었습니다.");
 });
